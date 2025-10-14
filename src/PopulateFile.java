e45b42cb77f45b8a3c0748d0c091778de42edf2c
Bazie521 <MTHBAZ001@myuct.ac.za>
2025-10-14 21:06:59 +0200

    File populating class developed and running well.

--- src/PopulateFile.java
+++ src/PopulateFile.java
@@ -1,12 +1,87 @@
-public class PopulateFile implements Runnable{
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.FileWriter;
 
-    private String password;
+public class PopulateFile {
 
-    public PopulateFile(String password){
-        this.password = password;
-    }
+    String tempPassword;
+    String fileName;
     
-    public void run(){
-        
+    public PopulateFile(){
+        tempPassword = "";
+        fileName = "dict.txt";
+    }
+
+    public PopulateFile(String fileName){
+        tempPassword = "";
+        this.fileName = fileName + ".txt";
+    }
+
+    public void populateFile(){
+
+        FileWriter myWriter = null;
+        Path filePath = Path.of("../files/" + fileName);
+
+        try{
+            Files.deleteIfExists(filePath);
+            System.out.println("Existing file " + fileName + " deleted.");
+        }
+
+        catch (Exception e){
+
+        }
+
+        try{
+            Files.createFile(filePath);
+            System.out.println("File " + fileName + " created successfully");
+        }
+
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+        try{
+            myWriter = new FileWriter(filePath.toFile(), true);
+        }
+
+        catch (Exception e){
+            System.out.println(e);
+            System.exit(0);
+        }
+ 
+        int count = 0;
+        for (String p: new String[] {"P", "p"}){
+            for (String a: new String[] {"A", "a", "@"}){
+                for (String s1: new String[] {"S", "s", "5"}){
+                    for (String s2: new String[] {"S", "s", "5"}){
+                        for (String w: new String[] {"W", "w"}){
+                            for (String o: new String[] {"O", "o", "0"}){
+                                for (String r: new String[] {"R", "r"}){
+                                    for (String d: new String[] {"D", "d"}){
+                                        tempPassword = p + a + s1 + s2 + w + o + r + d;
+                                        try{
+                                            myWriter.write(tempPassword+"\n");
+                                        }
+                                        catch(Exception e){
+                                            System.out.println(e);
+                                            System.exit(0);
+                                        }
+                                        count += 1;
+                                    }
+                                }                          
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        try{
+            myWriter.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+        System.out.println(count);
     }
 }
--- src/WarpDevMain.java
+++ src/WarpDevMain.java
@@ -1,16 +1,17 @@
 public class WarpDevMain{
 
-    public void generate_file(){
-
+    public static void generate_file(){
+        PopulateFile populateFileObj = new PopulateFile();
+        populateFileObj.populateFile();    
     }
 
     public void run_attack(){
-
+        
     }
     
     public static void main(String[] args){
         if (args[0].equals("generate_file")){
-            
+            generate_file();
         }
 
         else if(args[0].equals("run_attack")){
